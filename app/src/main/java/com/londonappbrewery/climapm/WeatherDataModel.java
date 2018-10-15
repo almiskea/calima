1ad6ef586025da387b585698c8bcef9643f77f2c
almiskea <ali.miskeen@gmail.com>
2018-10-15 12:11:41 -0700

    complete

--- app/src/main/java/com/londonappbrewery/climapm/WeatherDataModel.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherDataModel.java
@@ -1,47 +1,83 @@
 package com.londonappbrewery.climapm;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class WeatherDataModel {
 
     // TODO: Declare the member variables here
-
+    private String mTempreture;
+    private int mCondition;
+    private String mCity;
+    private String mIconName;
 
     // TODO: Create a WeatherDataModel from a JSON:
-
+    public static WeatherDataModel fromJson(JSONObject jsonObject) throws JSONException {
+        WeatherDataModel weatherData = new WeatherDataModel();
+        weatherData.mCity = jsonObject.getString("name");
+        weatherData.mCondition = jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
+        weatherData.mIconName = updateWeatherIcon(weatherData.mCondition);
+        double temp = jsonObject.getJSONObject("main").getDouble("temp") - 273.15;
+        weatherData.mTempreture = Integer.toString((int) Math.rint(temp));
+        return weatherData;
+    }
 
     // TODO: Uncomment to this to get the weather image name from the condition:
-//    private static String updateWeatherIcon(int condition) {
-//
-//        if (condition >= 0 && condition < 300) {
-//            return "tstorm1";
-//        } else if (condition >= 300 && condition < 500) {
-//            return "light_rain";
-//        } else if (condition >= 500 && condition < 600) {
-//            return "shower3";
-//        } else if (condition >= 600 && condition <= 700) {
-//            return "snow4";
-//        } else if (condition >= 701 && condition <= 771) {
-//            return "fog";
-//        } else if (condition >= 772 && condition < 800) {
-//            return "tstorm3";
-//        } else if (condition == 800) {
-//            return "sunny";
-//        } else if (condition >= 801 && condition <= 804) {
-//            return "cloudy2";
-//        } else if (condition >= 900 && condition <= 902) {
-//            return "tstorm3";
-//        } else if (condition == 903) {
-//            return "snow5";
-//        } else if (condition == 904) {
-//            return "sunny";
-//        } else if (condition >= 905 && condition <= 1000) {
-//            return "tstorm3";
-//        }
-//
-//        return "dunno";
-//    }
+    private static String updateWeatherIcon(int condition) {
+
+        if (condition >= 0 && condition < 300) {
+            return "tstorm1";
+        } else if (condition >= 300 && condition < 500) {
+            return "light_rain";
+        } else if (condition >= 500 && condition < 600) {
+            return "shower3";
+        } else if (condition >= 600 && condition <= 700) {
+            return "snow4";
+        } else if (condition >= 701 && condition <= 771) {
+            return "fog";
+        } else if (condition >= 772 && condition < 800) {
+            return "tstorm3";
+        } else if (condition == 800) {
+            return "sunny";
+        } else if (condition >= 801 && condition <= 804) {
+            return "cloudy2";
+        } else if (condition >= 900 && condition <= 902) {
+            return "tstorm3";
+        } else if (condition == 903) {
+            return "snow5";
+        } else if (condition == 904) {
+            return "sunny";
+        } else if (condition >= 905 && condition <= 1000) {
+            return "tstorm3";
+        }
+
+        return "dunno";
+    }
 
     // TODO: Create getter methods for temperature, city, and icon name:
 
 
+    public String getTempreture() {
+        return mTempreture + "°";
+    }
 
+    public void setTempreture(String tempreture) {
+        mTempreture = tempreture;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public void setCity(String city) {
+        mCity = city;
+    }
+
+    public String getIconName() {
+        return mIconName;
+    }
+
+    public void setIconName(String iconName) {
+        mIconName = iconName;
+    }
 }
